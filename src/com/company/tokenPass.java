2910153ab8f45e8daf1329dc16997480cdaea13a
Jacob Cedar <jcedar8640@bths.edu>
2018-11-21 11:46:55 -0500

    added stuff

--- src/com/company/tokenPass.java
+++ src/com/company/tokenPass.java
@@ -1,6 +1,11 @@
 package com.company;
 
 public class tokenPass {
-
+    public TokenPass(int playerCount){
+        board = new int[playerCount];
+        for(int i = 0; i < playerCount; i++){
+            board[i]
+        }
+    }
 
 }
