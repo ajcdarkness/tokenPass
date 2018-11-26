88cddc6e7332e8aa27201c8ec2585349f43f1931
ajcdarkness <33042703+ajcdarkness@users.noreply.github.com>
2018-11-26 18:49:36 -0500

    added most everything

--- src/com/company/tokenPass.java
+++ src/com/company/tokenPass.java
@@ -1,10 +1,24 @@
 package com.company;
 
 public class tokenPass {
-    public TokenPass(int playerCount){
+    private int[] board;
+    private int currentPlayer;
+    public void TokenPass(int playerCount){
         board = new int[playerCount];
         for(int i = 0; i < playerCount; i++){
-            board[i]
+            board[i] = 1 + (int) (10 * Math.random());
+        }
+
+    }
+    public void distributeCurrentPlayerTokens(){
+        int nextPlayer = currentPlayer;
+        int numToDistribute = board[currentPlayer];
+        board[currentPlayer] = 0;
+
+        while(numToDistribute > 0){
+            nextPlayer = (nextPlayer + 1) % board.length;
+            board[nextPlayer]++;
+            numToDistribute--;
         }
     }
 
